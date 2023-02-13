758bbb2bfa10568836ae0fde5138263a5f2b090f
steffen kroyer <stef5450@stud.kea.dk>
2023-02-13 15:51:13 +0100

    everting

---
+++ 3caPitalization/capitalization3.js
@@ -0,0 +1,13 @@
+"use strict";
+
+//const firstName = "peter";
+
+//console.log(firstName.toUpperCase());
+
+//const lastName = "JONSON";
+
+//console.log(lastName.toLowerCase());
+
+const firstName = "peter";
+
+console.log(firstName.substring(0, 2).toLowerCase() + firstName[2].toUpperCase() + firstName.substring(3).toLowerCase());
---
+++ Strings/strting.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const longline =
+  "A very, very, very, very, VERY! long line of text,\
+ that spans arcoss the with of the editor!";
+
+const name = "Peter";
+const drink = "Pepsi";
+
+console.log(`Hello ${name}, would you like a ${drink}?`);
+
+
