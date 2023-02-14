427999e4be8b3bb1ad2dce745b6ceaabd2b98cae
steffen kroyer <stef5450@stud.kea.dk>
2023-02-14 11:51:27 +0100

    add a commit to strting.js

--- Strings/strting.js
+++ Strings/strting.js
@@ -12,4 +12,8 @@ console.log(`Hello ${name}, would you like a ${drink}?`);
 const petName = "Mandu";
 const petType = "Cat";
 
-console.log(`My name is ${name}. \n I have a ${petType} called ${petName}`);
+console.log(`My name is ${name}.
+I have a ${petType} called ${petName}`);
+
+// it also be done this way, with the link \n but you don't have to use it.
+//console.log(`My name is ${name}.\nI have a ${petType} called ${petName}`);
