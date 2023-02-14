04d4b8f656c4d650de0992855487f630ef3dbc67
steffen kroyer <stef5450@stud.kea.dk>
2023-02-14 11:47:12 +0100

    small "tempale literal" exercise done

--- Strings/strting.js
+++ Strings/strting.js
@@ -9,4 +9,7 @@ const drink = "Pepsi";
 
 console.log(`Hello ${name}, would you like a ${drink}?`);
 
+const petName = "Mandu";
+const petType = "Cat";
 
+console.log(`My name is ${name}. \n I have a ${petType} called ${petName}`);
