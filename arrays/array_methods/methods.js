290d0192ea8e5dbb74cfbdb8f3d8f14b97f08520
steffen kroyer <stef5450@stud.kea.dk>
2023-02-17 15:58:01 +0100

    working om array methods

---
+++ arrays/array_methods/methods.js
@@ -0,0 +1,22 @@
+//You are going to test a lot of different array methods
+//Start with this array:
+const people = ["Harry", "Ron", "Hermione"];
+
+//And an empty variable to store a result:
+let result;
+//For each of the following methods, store the return value in the result,
+//and note how the complete array looks, and what is returned. Like this:
+result = people.push("Draco");
+
+//result = people.pop();
+//result = people.push("Neville");
+//result = people.push("Luna");
+//result = people.slice(0, 3);
+//result = people.splice(1, 0, "Cho");
+//people[1] = "Ginny";
+//result = people.push("Fred", "George");
+//result = people.indexOf("Fred");
+//result = people.splice(result, 1);
+
+console.log(result);
+console.log(people);
