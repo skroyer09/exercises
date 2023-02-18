6067e627d884f50d0d9ce281380a9920d5fd3e4e
steffen kroyer <stef5450@stud.kea.dk>
2023-02-18 13:09:16 +0100

    convert done

---
+++ arrays/convert_to_arrays/convert.js
@@ -0,0 +1,12 @@
+const letters = Array.from("abcdefghijklmn");
+
+console.log(letters);
+
+const str = "abcdefghijklmn";
+const arr1 = str.split();
+
+const arr2 = Array.from(str);
+
+console.log(str);
+console.log(arr1);
+console.log(arr2);
