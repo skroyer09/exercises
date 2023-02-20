020670dcce68a7262745a5c34ba03e2d351c6f0f
steffen kroyer <stef5450@stud.kea.dk>
2023-02-20 14:00:03 +0100

    a lot for loops

---
+++ loops/manual_loop.js
@@ -0,0 +1,16 @@
+counter = 0;
+
+counter++;
+
+if (counter < 40) {
+  setTimeout(loop, 400);
+}
+
+//This code sets a variable counter to 0, and then increments it by 1 using the counter++ statement.
+
+//The if statement checks if the value of counter is less than 40. If it is,
+//the setTimeout function is called with two arguments: the first argument is the name of a
+//function called loop, and the second argument is a time delay of 400 milliseconds.
+
+//This code creates a loop that executes the loop function repeatedly, with a delay of
+//400 milliseconds between each execution, until the counter variable reaches a value of 40.
